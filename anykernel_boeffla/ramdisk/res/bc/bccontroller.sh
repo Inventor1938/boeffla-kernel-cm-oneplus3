e7cf972410ece7683fb950575f7d54083f170f14
andip71 <andreasp@gmx.de>
2016-11-17 15:12:05 +0100

    Boeffla ramfs: Add filename template for kernel download

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -13,7 +13,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="oneplus3;cm;cm14.0;http://kernel.boeffla.de/oneplus3/boeffla-kernel-cm/"
+KERNEL_SPECS="oneplus3;cm;cm14.0;http://kernel.boeffla.de/oneplus3/boeffla-kernel-cm/;boeffla-kernel-#VERSION#-CM14.1-OnePlus3-anykernel.recovery.zip"
 
 # kernel features 
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
