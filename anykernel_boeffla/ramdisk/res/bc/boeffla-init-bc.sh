bafe558e7dd6ac979ceba40dfe4d93703a5a5e69
andip71 <andreasp@gmx.de>
2017-01-05 17:48:37 +0100

    Boeffla ramfs: name change to lineageos

--- anykernel_boeffla/ramdisk/res/bc/boeffla-init-bc.sh
+++ anykernel_boeffla/ramdisk/res/bc/boeffla-init-bc.sh
@@ -3,7 +3,7 @@
 # *******************************
 # BC-based initialization
 #
-# Oneplus 3 CM14 version
+# Oneplus 3 Lineage14 version
 #
 # V0.1
 # *******************************
@@ -74,7 +74,7 @@
 
 	echo $(date) Boeffla-Kernel default settings applied >> $BOEFFLA_LOGFILE
 
-# init.d support (enabler only to be considered for CM based roms)
+# init.d support (enabler only to be considered for Lineage based roms)
 # (zipalign scripts will not be executed as only exception)
 	if [ -f $INITD_ENABLER ] ; then
 		echo $(date) Execute init.d scripts start >> $BOEFFLA_LOGFILE
